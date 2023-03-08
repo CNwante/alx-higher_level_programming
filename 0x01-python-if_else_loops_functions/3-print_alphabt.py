90c244a69563b5171e2050d8177460fe0e3e4ec3
CNwante <myphilothea@gmail.com>
2023-03-08 14:51:33 -0800

    Refactor: write code according to task requirment
    Use f-string formating style
    
    Now use .format() style

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -6,4 +6,4 @@ for alphabet in range(97, 123):
     if (alphabet == 101) or (alphabet == 113):
         continue
 
-    print(f"{chr(alphabet)}", end="")
+    print("{}".format(chr(alphabet)), end="")
