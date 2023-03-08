932f840a359e67646abf9c84146c4824c37b8819
CNwante <myphilothea@gmail.com>
2023-03-08 15:58:43 -0800

    Refactor: modify code for efficency

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,7 @@
 """ a program that outputs numbers from 0 to 99  """
 
 for number in range(0, 100):
+    print("{:02}".format(number), end=", ")
+
     if number == 99:
         print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
