3c9091de2ff3a70f9e9d0fefe5981563790fd2bd
CNwante <myphilothea@gmail.com>
2024-01-03 19:38:50 -0800

    Task(12): fizzbuzz algorithm

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    """ Fizz Buzz """
+    for num in range(1, 101):
+        if num % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif num % 3 == 0:
+            print("Fizz", end=" ")
+        elif num % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(num, end=" ")
