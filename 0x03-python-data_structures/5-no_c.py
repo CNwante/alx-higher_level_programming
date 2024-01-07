2b240bc281ff6f0c7298dd682dea6570e5613dc5
CNwante <myphilothea@gmail.com>
2024-01-06 16:47:03 -0800

    Refactor the code implementation
    - Code works fine but checker not accepting it

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,4 @@
 
 def no_c(my_string):
     """Removes all characters 'c' and 'C' from a string."""
-    str_list = [char for char in my_string if char not in ['c', 'C']]
-    new_str = ''.join(str_list)
-    return new_str
+    return ("".join([char for char in my_string if char not in ['c', 'C']]))
