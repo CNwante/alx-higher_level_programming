4ebb2cf235d2198bca0f904fd2dbe6329fef0aba
CNwante <myphilothea@gmail.com>
2024-01-06 16:41:53 -0800

    Fix small bug
     -Forgot the char variable

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 
 def no_c(my_string):
     """Removes all characters 'c' and 'C' from a string."""
-    str_list = [char for char in my_string if not in ['c', 'C']]
+    str_list = [char for char in my_string if char not in ['c', 'C']]
     new_str = ''.join(str_list)
     return new_str
