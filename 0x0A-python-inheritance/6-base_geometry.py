83c7009b37b3e7e6d1e95f7601f011f01d306974
CNwante <myphilothea@gmail.com>
2024-02-07 04:15:40 -0800

    Task(6): improve BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Auth: Solomon Nwante
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """ Rase: [Exception] area() is not implemented. """
+        raise Exception("area() is not implemented")
