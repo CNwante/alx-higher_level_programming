4be0953382a0b4471a8d06faf17e193ddac5d447
CNwante <myphilothea@gmail.com>
2024-02-10 20:05:19 -0800

    File: add rectangle.py

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# test_rectangle.py
+# Auth: Solomon Nwante
+"""Defines unittests for models/rectangle.py.
+
+Unittest classes:
+    TestRectangle_instantiation
+"""
+import io
+import sys
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
